2f72eb995dc8fb5d1e5806c4ef13a71f662e6a34
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2024-03-20 16:37:50 +1100

    add stores in price optimsation tool page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,33 @@ function App() {
     <Router>
       <div className="App">
         {/* <Header /> */}
-        <main>
+        {/* Navbar */}
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">Price Optimisation Tools</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a>Link</a>
+              </li>
+              <li>
+                <details>
+                  <summary>Parent</summary>
+                  <ul className="p-2 bg-base-100 rounded-t-none">
+                    <li>
+                      <a>Link 1</a>
+                    </li>
+                    <li>
+                      <a>Link 2</a>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <main className="m-4">
           <Routes />
         </main>
         {/* <Footer /> */}
