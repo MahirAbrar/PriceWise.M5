ae07209305edf1d1e1da872a6dab63b4c0daf263
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2024-05-16 17:18:04 +1000

    added user guide button

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -14,20 +14,37 @@ const LandingPage = () => {
     navigate("/select");
   };
 
+  const handleUserGuide = () => {
+    // how do i open a new tab to a different website
+    window.open(
+      "https://docs.google.com/document/d/1y-oB1fr-GIIQ46CIuPHX0XslUofDwTVR_dgD-jMOP8I/edit?usp=sharing"
+    );
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="bg-white p-10 rounded-3xl">
-        <h1 className="font-bold text-xl mb-4">Welcome, Let's Get Started!</h1>
-        <div>
-          <button
-            className="btn btn-wide btn-primary rounded-full"
-            onClick={handleStartClick}
-          >
-            Start
-          </button>
+    <>
+      <button
+        className="btn btn-wide btn-success rounded-full"
+        onClick={handleUserGuide}
+      >
+        User guide
+      </button>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="bg-white p-10 rounded-3xl">
+          <h1 className="font-bold text-xl mb-4">
+            Welcome, Let's Get Started!
+          </h1>
+          <div>
+            <button
+              className="btn btn-wide btn-primary rounded-full"
+              onClick={handleStartClick}
+            >
+              Start
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
